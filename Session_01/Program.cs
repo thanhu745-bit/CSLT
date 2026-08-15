f543c1983ea33cde71261a6114e7d4fcd848c5bd
thanhu745-bit <thanhu745@gmail.con>
2026-08-16 01:11:54 +0700

    Add project files.

---
+++ Session_01/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Thanh Uyên");
+        Console.WriteLine("I am a student at the University of Economics, Ho Chi Minh City (UEH)");
+    }
+}
