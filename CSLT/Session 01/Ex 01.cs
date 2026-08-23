e587ce9c2befdd910b7cef5c84b3fe565f0ef596
thanhu745-bit <thanhu745@gmail.con>
2026-08-23 22:17:30 +0700

    Restructure repository: Move sessions inside CSLT project

---
+++ CSLT/Session 01/Ex 01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLT.Session_01
+{
+    internal class Ex_01
+    {
+        private static void Main_S01(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Thanh Uyên");
+            Console.WriteLine("I am a student at the University of Economics, Ho Chi Minh City (UEH)");
+            Console.WriteLine("I am currently learning C# programming language");
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ CSLT/Session 03/Ex 01.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSLT.Session_03
+{
+    internal class Ex_01
+    {
+    }
+}
